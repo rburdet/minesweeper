90a23a18748979f2259a0873e0fc2ccd3a76f93a
Rodrigo Burdet <rodrigo.burdet@rappi.com>
2020-04-24 00:15:33 -0300

    Adding game handler

---
+++ server/api/game/handler.go
@@ -0,0 +1,42 @@
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type ClickAction struct {
+	I      int
+	J      int
+	Action string `binding:oneof="flag question"`
+}
+
+type GameHandler struct {
+	service *GameService
+}
+
+func NewGameHandler(s *GameService) *GameHandler {
+	return &GameHandler{s}
+}
+
+func (gh *GameHandler) CreateGame(c *gin.Context) {
+	newGame := NewGame()
+	if err := c.ShouldBindJSON(&newGame); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	newGame.fillBoard()
+	if gh.service.Exists(newGame.Name) {
+		c.JSON(http.StatusForbidden, gin.H{"error": "already exists"})
+		return
+	}
+	gh.service.SaveGame(newGame)
+
+	c.JSON(http.StatusOK, gin.H{"id": newGame.Name})
+}
+
+func (gh *GameHandler) GetGame(c *gin.Context) {
+	name := c.Param("name")
+	savedGame := gh.service.GetGame(name)
+
+	c.JSON(http.StatusOK, savedGame)
+}
